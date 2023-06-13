9a1f0f928b629fa0abb391d43698b12ef82f3ea1
zxcvgx <97776436+zxcvgx@users.noreply.github.com>
2023-06-13 18:18:03 +0800

    Update Program.cs
    Added advance menu control.
    Added Drag and Drop file feature.
    
    Reminder: Don't forget to change the file path of your sound file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Ports;
 using System.Linq;
@@ -8,19 +9,76 @@ namespace AECA
 {
     class Program
     {
-        static void Main(string[] args)
+        private static int users_option = 0;
+        private static string excelFilePath = @"";
+
+        public static void Main(string[] args)
         {
-            Console.Title = "Arduino To Excel Communication App (AECA)";
+            Console.Title = "Arduino To Excel Communication App (AECA)   |   MENU";
+
+            // Check if a file was dragged and dropped
+            if (args.Length > 0)
+            {
+                excelFilePath = args[0];
+            }
+
             // Set up the serial port to communicate with Arduino
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
             SerialPort serialPort = new SerialPort("COM6", 9600);
             serialPort.DataReceived += SerialPort_DataReceived;
-            serialPort.Open();
 
-            Console.WriteLine("Waiting for data from Arduino...");
+            bool exitProgram = false;
 
-            Console.ReadLine();
-            serialPort.Close();
+            while (!exitProgram)
+            {
+                Console.Clear();
+                Console.Title = "Arduino To Excel Communication App (AECA)   |   MENU";
+                Console.WriteLine(" \n MENU");
+                Console.WriteLine(" (1) Read Card / Tag UID");
+                Console.WriteLine(" (2) Log Data");
+                Console.WriteLine(" (3) Open Logs");
+                Console.WriteLine(" (4) Exit");
+
+                var option = Console.ReadKey().KeyChar.ToString();
+                Console.Clear();
+
+                switch (option)
+                {
+                    case "1":
+                        Console.Title = "Arduino To Excel Communication App (AECA)   |   MENU >  Read Card/Tag UID";
+                        users_option = 1;
+                        serialPort.Open();
+                        Console.WriteLine(" \n Scan your card!\n");
+                        Console.ReadKey();
+                        serialPort.Close();
+                        break;
+
+                    case "2":
+                        Console.Title = "Arduino To Excel Communication App (AECA)   |   MENU >  Log Data";
+                        users_option = 2;
+                        serialPort.Open();
+                        Console.WriteLine(" \n Scan your card!\n");
+                        Console.ReadKey();
+                        serialPort.Close();
+                        break;
+
+                    case "3":
+                        Console.Title = "Arduino To Excel Communication App (AECA)   |   MENU >  Open Logs";
+                        Console.WriteLine(" \n Opening Logs!\n");
+                        Process.Start(excelFilePath);
+                        Console.ReadKey();
+                        break;
+
+                    case "4":
+                        exitProgram = true;
+                        break;
+
+                    default:
+                        Console.WriteLine(" \n Invalid option! Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+                }
+            }
         }
 
         private static void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
@@ -30,43 +88,56 @@ namespace AECA
             string receivedData = serialPort.ReadLine();
 
             // Find the row and column in Excel with the received UID
-            string uidToFind = receivedData.Trim();
-	    //Make sure to add the UID to the 3rd column
-            string excelFilePath = @"C:\Users\JeanC\source\repos\Arduino To Excel Communication\bin\Release\Book1.xlsx";
+            string uid = receivedData.Trim();
 
-            using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFilePath)))
+            if (users_option == 1)
             {
-                foreach (ExcelWorksheet worksheet in package.Workbook.Worksheets)
+                Console.WriteLine(" UID: " + uid);
+                System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                player.SoundLocation = @"C:\Users\JeanC\Music\achive-sound-132273.wav";
+                player.Play();
+                player.Dispose();
+            }
+            else if (users_option == 2)
+            {
+                using (ExcelPackage package = new ExcelPackage(new FileInfo(excelFilePath)))
                 {
-                    ExcelRangeBase range = worksheet.Cells[worksheet.Dimension.Address];
-                    ExcelRangeBase uidCell = range.FirstOrDefault(cell => cell.Value != null && cell.Value.ToString() == uidToFind);
-
-                    if (uidCell != null)
+                    foreach (ExcelWorksheet worksheet in package.Workbook.Worksheets)
                     {
-                        // Get the row number of the found UID cell
-                        int rowToWriteIndex = uidCell.Start.Row;
+                        ExcelRangeBase range = worksheet.Cells[worksheet.Dimension.Address];
+                        ExcelRangeBase uidCell = range.FirstOrDefault(cell => cell.Value != null && cell.Value.ToString() == uid);
 
-                        // Write the current date and time next to the found column
-                        int columnToWriteIndex = uidCell.Start.Column + 1;  // write in the next column
+                        if (uidCell != null)
+                        {
+                            // Get the row number of the found UID cell
+                            int rowToWriteIndex = uidCell.Start.Row;
 
-                        ExcelRangeBase cellToWrite = worksheet.Cells[rowToWriteIndex, columnToWriteIndex];
+                            // Write the current date and time next to the found column
+                            int columnToWriteIndex = uidCell.Start.Column + 1;  // write in the next column
 
-                        // Get the current date and time in the desired format
-                        string currentDate = DateTime.Now.ToString("dddd, MMMM d, yyyy - h:mm tt");
+                            ExcelRangeBase cellToWrite = worksheet.Cells[rowToWriteIndex, columnToWriteIndex];
 
-                        cellToWrite.Value = currentDate;
+                            // Get the current date and time in the desired format
+                            string currentDate = DateTime.Now.ToString("dddd, MMMM d, yyyy - h:mm tt");
 
-                        // Retrieve the data in the row and print it
-                        object[] rowData = worksheet.Cells[rowToWriteIndex, 1, rowToWriteIndex, worksheet.Dimension.Columns].Select(cell => cell.Value).ToArray();
-                        string rowDataString = string.Join(" | ", rowData);
+                            cellToWrite.Value = currentDate;
 
-                        // Print the data in the row along with the date and time
-                        Console.WriteLine($"\nData written successfully \n{rowDataString}");
+                            // Retrieve the data in the row and print it
+                            object[] rowData = worksheet.Cells[rowToWriteIndex, 1, rowToWriteIndex, worksheet.Dimension.Columns].Select(cell => cell.Value).ToArray();
+                            string rowDataString = string.Join(" | ", rowData);
+
+                            // Print the data in the row along with the date and time
+                            Console.WriteLine($"\n Data logged and written successfully! \n {rowDataString}");
+                            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
+                            player.SoundLocation = @"C:\Users\JeanC\Music\achive-sound-132273.wav";
+                            player.Play();
+                            player.Dispose();
+                        }
                     }
-                }
 
-                // Save the changes to the Excel file
-                package.Save();
+                    // Save the changes to the Excel file
+                    package.Save();
+                }
             }
         }
     }
